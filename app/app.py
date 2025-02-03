a46094dfe66a66d5e39de45cd797ccfa6813e8b1
SteveP <srp1010@yahoo.co.uk>
2025-02-03 15:13:14 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Hello World! - version 1.2.0"
+	return "Hello World! - version 1.3.0"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
